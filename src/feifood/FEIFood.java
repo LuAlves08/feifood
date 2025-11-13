a5bacc2db72695c75dfaa8e06a6a2b2d9fb58d46
Lucas <Lucas@DESKTOP-RGKDMTN>
2025-11-13 10:03:45 -0300

    login

---
+++ src/feifood/FEIFood.java
@@ -0,0 +1,9 @@
+package feifood;
+
+import view.Login;
+
+public class FEIFood {
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(() -> new Login().setVisible(true));
+    }
+}
